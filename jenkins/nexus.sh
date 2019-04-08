267044a4b354437e2d3674bbe3f0e1264c3cb6b8
olfize <olfize@10.0.75.1>
2019-04-08 17:06:35 +0200

    Commit a change

---
+++ jenkins/nexus.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo 'The following script command add the java application to the Nexus'
+set -x
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+set +x
+
+echo 'The following complex command extracts the value of the <name/> element'
+echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+set -x
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+set +x
+
+echo 'The following complex command behaves similarly to the previous one but'
+echo 'extracts the value of the <version/> element within <project/> instead.'
+set -x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
+
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
+curl -u admin:admin123 --upload-file target/*.jar http://nexus:8081/repository/com.mycompany.app/${NAME}-${VERSION}.jar

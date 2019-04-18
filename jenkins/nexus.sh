947177a1a9d1ec009c12d49ac6e3267ca5d1eaff
olfize <olfize@10.0.75.1>
2019-04-18 11:39:21 +0200

    put mvn in sh

--- jenkins/nexus.sh
+++ jenkins/nexus.sh
@@ -20,4 +20,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI...'
 set -x
-curl -u admin:admin123 --upload-file target/${NAME}-${VERSION}.war http://nexus:8081/repository/com.pokebible/${NAME}-${VERSION}.war
+curl -u admin:admin123 --upload-file target/${NAME}-${VERSION}.war http://pgnexus:8081/repository/com.pokebible/${NAME}-${VERSION}.war
